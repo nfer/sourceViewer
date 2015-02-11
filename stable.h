583c86d4017cd439c12c33b459d05cfcee291cfe
Nfer zhuang <nfer.zhuang@gmail.com>
2015-02-11 19:04:19 +0800

    add __cplusplus define protect on stable.h

--- stable.h
+++ stable.h
@@ -1,9 +1,11 @@
 #ifndef STABLE_H
 #define STABLE_H
 
+#if defined __cplusplus
 #include <QApplication>
 #include <QMainWindow>
 #include <QtWidgets>
 #include <QDebug>
+#endif // __cplusplus
 
 #endif // STABLE_H
